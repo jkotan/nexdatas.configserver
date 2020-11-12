dc85259f5af49a13ac26dfebd77def5f9133bcf7
Jan Kotanski <jankotan@gmail.com>
2020-11-12 17:30:17 +0100

    add ubuntu20.04 back

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,11 +4,14 @@
 if [ $1 = "ubuntu16.04" ]; then
     docker exec -it --user root ndts sed -i "s/\[mysqld\]/\[mysqld\]\nsql_mode = NO_ZERO_IN_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION/g" /etc/mysql/mysql.conf.d/mysqld.cnf
 fi
-if [ $1 = "ubuntu20.04" ]; then
+if [ $1 = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ]; then
     docker exec -it --user root ndts sed -i "s/\[mysql\]/\[mysqld\]\nsql_mode = NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION\ncharacter_set_server=latin1\ncollation_server=latin1_swedish_ci\n\[mysql\]/g" /etc/mysql/mysql.conf.d/mysql.cnf
 fi
 
 docker exec  --user root ndts service mysql stop
+if [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ]; then
+    docker exec -it --user root ndts /bin/bash -c 'sudo usermod -d /var/lib/mysql/ mysql'
+fi
 docker exec  --user root ndts /bin/sh -c '$(service mysql start &) && sleep 30'
 
 docker exec  --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y   tango-db tango-common; sleep 10'
@@ -32,7 +35,7 @@ if [ $2 = "2" ]; then
     docker exec  --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python-pytango  python-mysqldb python-setuptools'
 else
     echo "install python3-pytango"
-    if [ $1 = "ubuntu20.04" ]; then
+    if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "debian11" ] ; then
 	docker exec --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python3-tango python3-mysqldb python3-setuptools'
     else
 	docker exec  --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python3-pytango python3-mysqldb python3-setuptools'
