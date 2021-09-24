b339f89143e832279c7699eb53f65f3fe38ee118
Jan Kotanski <jankotan@gmail.com>
2021-09-24 14:45:50 +0200

    add debian11 tests and weekly cron job

--- .ci/run.sh
+++ .ci/run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [ $1 = "2" ]; then
+if [ "$1" = "2" ]; then
     echo "run python-nxsconfigserver"
     docker exec  ndts python test
 else
     echo "run python3-nxsconfigserver"
     docker exec  ndts python3 test
 fi    
-if [ $? -ne "0" ]
+if [ "$?" -ne "0" ]
 then
     exit -1
 fi
