7ca7cfb9df5dd24784ba5c048b1059a4f055b079
Jan Kotanski <jankotan@gmail.com>
2021-11-19 11:06:25 +0100

    change tests to pytest

--- .ci/run.sh
+++ .ci/run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-
-if [ "$1" = "2" ]; then
-    echo "run python-nxsconfigserver"
-    docker exec  ndts python test
+if [ "$2" = "2" ]; then
+    echo "run python-nxsconfigserver tests"
+    docker exec ndts python test
 else
-    echo "run python3-nxsconfigserver"
-    docker exec  ndts python3 test
+    echo "run python3-nxsconfigserver tests"
+    if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.04" ] || [ "$1" = "debian11" ] ; then
+	docker exec ndts python3 -m pytest
+    else
+	docker exec ndts python3 test
+    fi
 fi    
-if [ "$?" -ne "0" ]
-then
-    exit -1
-fi
+if [ "$?" != "0" ]; then exit -1; fi
